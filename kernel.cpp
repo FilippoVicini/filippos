515c12484c70c86ba87ed17d1ff1e9cb759df6d8
Filippo Vicini <filippo.vicini2@gmail.com>
2024-05-02 11:45:52 +0100

    kernel print fix for memory location

--- kernel.cpp
+++ kernel.cpp
@@ -1,6 +1,15 @@
+
+void printf(char* str)
+{ 
+	unsigned short* VideoMemory = (unsigned short*)0xb8000;
+	
+
+	for(int i = 0; str[i] != '\0';++i)
+		VideoMemory[i] = (VideoMemory[i] & 0xF00) | str[i];
+}
 void kernerlMain(void* multiboot_structure, unsigned int magicnumber){
 	printf("Hello world this is FilipOS");
 	
-	while();
+	while(1);
 }
 
